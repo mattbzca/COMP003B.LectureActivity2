fccee6886863f3df44770d77f54a2cd61622a976
Matthew Tan <matthewtan@my.whccd.edu>
2025-03-24 18:32:27 -0700

    Add project files.

---
+++ COMP003B.LectureActivity2/Program.cs
@@ -0,0 +1,50 @@
+/*
+ * Author: Matthew Tan
+ * Course: COMP003B
+ * Faculty: Jonathan Cruz
+ * Purpose: My second ASP.NET Core application using MVC.
+ * 
+ */ 
+
+namespace COMP003B.LectureActivity2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //Middleware sequence
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<COMP003B.LectureActivity2.Middleware.RequestLoggingMiddleware>();
+
+            app.UseWelcomePage("/welcome");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
